c32f1b1d266d1644d10b6e381f6d01e354d32146
EnzoMarinich <marinichenzo@gmail.com>
2023-07-17 15:36:56 -0300

    probando

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Link from "next/link"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1>navbar</h1>
+        <ul>
+        <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/actividades">Actividades</Link>
+          </li>
+        </ul>
+        {children}
+      </body>
     </html>
   )
 }
