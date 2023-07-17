f59c0cd880ad97782fab77446b64db4b8f99bc7e
Denise <deniselucianarosado@gmail.com>
2023-07-17 15:10:43 -0300

    cambios

---
+++ client/app/page.jsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <h1>holi</h1>
+  )
+}
---
+++ client/pages/api/random.js
@@ -0,0 +1,5 @@
+export default function handler(req, res) {
+    const randomNumber = Math.floor(Math.random() * 100); // Generate a random number between 0 and 100
+  
+    res.status(200).json({ number: randomNumber });
+  }
