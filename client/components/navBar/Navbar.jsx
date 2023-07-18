db73128b3407e19461c48123e627a109a04f77dd
EnzoMarinich <marinichenzo@gmail.com>
2023-07-18 18:25:52 -0300

    nav casi listo

--- client/components/navBar/Navbar.jsx
+++ client/components/navBar/Navbar.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import Link from "next/link"
-
+import React from "react";
+import Link from "next/link";
+import style from "./Navbar.module.css";
 
 export const Navbar = () => {
-
   const obj = [
-  {label : "home", route: "/"},
-  {label : "About", route: "/aboutUs"},
-  {label : "Contact", route: "/contact"},
-  {label : "My turns", route: "/turns"},
-  {label : "Login", route: "/login"}
-  ]
+    { label: "Home", route: "/" },
+    { label: "About", route: "/aboutUs" },
+    { label: "Contact", route: "/contact" },
+    { label: "My turns", route: "/turns" },
+    { label: "Login", route: "/login" },
+  ];
 
   return (
-    <div>
-        <ul>
-            {obj.map(({label, route})=>{
-              return (<li key={route}>
+    <div className={style.container}>
+      <div className={style.options}>
+        <ul className={style.ul}>
+          {obj.map(({ label, route }) => {
+            return (
+              <li key={route}>
                 <Link href={route}>{label}</Link>
-              </li>)
-            })}
+              </li>
+            );
+          })}
         </ul>
+      </div>
+      <div>lupita</div>
     </div>
-  )
-}
+  );
+};
