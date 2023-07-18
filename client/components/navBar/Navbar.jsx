ad4f39121e32a27a9c0fa1ee182c0405ff4ec542
EnzoMarinich <marinichenzo@gmail.com>
2023-07-18 15:49:03 -0300

    add route login

--- client/components/navBar/Navbar.jsx
+++ client/components/navBar/Navbar.jsx
@@ -9,9 +9,9 @@ export const Navbar = () => {
   {label : "About", route: "/aboutUs"},
   {label : "Contact", route: "/contact"},
   {label : "My turns", route: "/turns"},
-  {label : "Login", route: "/logIn"}
+  {label : "Login", route: "/login"}
   ]
-  
+
   return (
     <div>
         <ul>
