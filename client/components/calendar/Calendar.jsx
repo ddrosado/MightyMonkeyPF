74ee63a942fb2a3f0cf4a4c09ac00e19ec9f9dba
gonzalo00913 <gonzalo-masa@outlook.com>
2023-07-19 15:31:23 -0300

    detail

--- client/components/calendar/Calendar.jsx
+++ client/components/calendar/Calendar.jsx
@@ -1,27 +1,74 @@
-import React from "react";
+"use client";
+import React, { useState } from 'react';
 import style from "./Calendar.module.css";
 
 const Calendar = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const getDaysInMonth = (year, month) => {
+    return new Date(year, month + 1, 0).getDate();
+  };
+
+  const monthNames = [
+    "Ene", "Feb", "Mar", "Abr", "May", "Jun",
+    "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"
+  ];
+
+  const currentMonth = currentDate.getMonth();
+  const currentYear = currentDate.getFullYear();
+  const daysInMonth = getDaysInMonth(currentYear, currentMonth);
+
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+  const emptyDays = Array.from({ length: firstDayOfMonth }, (_, index) => (
+    <span className={style.emptyDay} key={`empty-${index}`} />
+  ));
+
+  const daysOfMonth = Array.from({ length: daysInMonth }, (_, index) => (
+    <span className={style.calendarDay} key={index + 1}>
+      {index + 1}
+    </span>
+  ));
+
+
+  const handleNextMonth = () => {
+    setCurrentDate(prevDate => {
+      const newDate = new Date(prevDate);
+      newDate.setMonth(prevDate.getMonth() + 1);
+      return newDate;
+    });
+  };
+
+  const handlePreviousMonth = () => {
+    setCurrentDate(prevDate => {
+      const newDate = new Date(prevDate);
+      newDate.setMonth(prevDate.getMonth() - 1);
+      return newDate;
+    });
+  };
+
   return (
     <div className={style.calendarContainer}>
-      <h3 className={style.h3}>Jun</h3>
+      <h3 className={style.h3}>{monthNames[currentMonth]}</h3>
       <div className={style.calendarHeaderContainer}>
-        <span className={style.containerDay}>Mo</span>
-        <span className={style.containerDay}>Tu</span>
-        <span className={style.containerDay}>We</span>
-        <span className={style.containerDay}>Th</span>
-        <span className={style.containerDay}>Fr</span>
-        <span className={style.containerDay}>Sa</span>
-        <span className={style.containerDay}>Su</span>
+        <span className={style.containerDay}>Lu</span>
+        <span className={style.containerDay}>Ma</span>
+        <span className={style.containerDay}>Mi</span>
+        <span className={style.containerDay}>Ju</span>
+        <span className={style.containerDay}>Vi</span>
+        <span className={style.containerDay}>Sá</span>
+        <span className={style.containerDay}>Do</span>
       </div>
       <div className={style.calendarDaysContainer}>
-        {Array.from({ length: 31 }, (_, index) => (
-          <span className="calendar-day" key={index + 1}>
-            {index + 1}
-          </span>
-        ))}
+        {emptyDays}
+        {daysOfMonth}
       </div>
-      <button className={style.button}>Reserva</button>
+      <button className={style.button} onClick={handlePreviousMonth}>
+        Mes anterior
+      </button>
+      <button className={style.button} onClick={handleNextMonth}>
+        Mes siguiente
+      </button>
+      <button>Reservation</button>
     </div>
   );
 };
