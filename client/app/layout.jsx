b0d4e0ab83e3a08a5148873a5c3ef62940bae5b7
EnzoMarinich <marinichenzo@gmail.com>
2023-07-19 15:56:34 -0300

    create form contact

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Link from "next/link";
 import { Navbar } from "../components/navBar/Navbar";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,10 +11,12 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        <Navbar />
         {children}
       </body>
     </html>
