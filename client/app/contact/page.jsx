65450e99fe0bc8c74680488fd70565ea5ee81e60
EnzoMarinich <marinichenzo@gmail.com>
2023-07-19 14:12:43 -0300

    form created

--- client/app/contact/page.jsx
+++ client/app/contact/page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import FormContact from '../../components/formContact/FormContact'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <h1>Contact</h1>
+      <FormContact/>
+    </div>
   )
 }
 
