7f4f5cf001c9000720ff1e6b844c557d21771926
LucasFigueroa5 <lucasfigueroab@gmail.com>
2023-07-20 17:12:49 -0300

    css general

--- client/app/contact/page.jsx
+++ client/app/contact/page.jsx
@@ -1,16 +1,13 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import FormContact from "../../components/contact/formContact/FormContact";
 import style from "./page.module.css";
-import img from "../../assets/images/monoclub.jpg";
 import { SectionDate } from "../../components/contact/sectionDate/SectionDate";
 
 const Contact = () => {
   return (
     <div className={style.container}>
       <div className={style.portada}>
-        <Image src={img} alt='mono'/>
         <h1 className={style.title}>Contact</h1>
       </div>
       <div className={style.sectionForm}>
--- client/components/navBar/Navbar.jsx
+++ client/components/navBar/Navbar.jsx
@@ -2,31 +2,33 @@ import React from "react";
 import Link from "next/link";
 import style from "./Navbar.module.css";
 import Image from "next/image";
-import logo from '../../assets/images/logo.png'
+import logo from "../../assets/images/logo.png";
 
 export const Navbar = () => {
   const obj = [
-  {label : "home", route: "/home"},
-  {label : "About", route: "/aboutUs"},
-  {label : "Contact", route: "/contact"},
-  {label : "My turns", route: "/turns"},
-  {label : "Login", route: "/login"}
-  ]
-  
+    { label: "Home", route: "/home" },
+    { label: "About", route: "/aboutUs" },
+    { label: "Contact", route: "/contact" },
+    { label: "My turns", route: "/turns" },
+    { label: "Login", route: "/login" },
+  ];
+
   return (
-    <div className={style.container}>
-      <Image className={style.logo} src={logo} alt='#' />
-      <div className={style.options}>
-        <ul className={style.ul}>
-          {obj.map(({ label, route }) => {
-            return (
-              <li key={route}>
-                <Link href={route}>{label}</Link>
-              </li>
-            );
-          })}
-        </ul>
+    <div className={style.navContainer}>
+      <Image className={style.logo} src={logo} alt="#" />
+      <div className={style.barContainer}>
+        <div className={style.options}>
+          <ul className={style.ul}>
+            {obj.map(({ label, route }) => {
+              return (
+                <li key={route}>
+                  <Link href={route}>{label}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
