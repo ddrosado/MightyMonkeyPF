c82300516095eda244dd0dbd7ce82958b6d0d7e0
Facundo nuno <103080547+FacundoNuno@users.noreply.github.com>
2023-07-20 17:59:07 -0300

    asdasd

--- client/pages/api/controllers/user/getAllUsers.js
+++
@@ -1,9 +0,0 @@
-const {db} = require('../../db')
-db.sequelize.sync({force: true})
-const {User} = db
-
-
-module.exports = async () => { 
-    const users = await  User.findAll()
-    return users
-}
