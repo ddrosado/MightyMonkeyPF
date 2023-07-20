4d5a95fa80b0e539042e6223884d662c1ff0cef9
gonzalo00913 <gonzalo-masa@outlook.com>
2023-07-20 19:17:33 -0300

    cambios css detail

--- client/components/detail/Detail.jsx
+++ client/components/detail/Detail.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import style from "./Detail.module.css";
 import Calendar from "../calendar/Calendar";
 import Carrusel from "../carrusel/Carrusel";
+import Footer from "../footer/Footer";
 
 const Detail = () => {
   return (
-    <div className={style.detailContainer}>
+    <div  className={style.detailContainer}>
       <h2 className={style.title}>Alquiler</h2>
+    <div>
       <div className={style.containerImgCalendar}>
         <div>
           <div className={style.imagen}>image</div>
@@ -16,7 +18,7 @@ const Detail = () => {
           <Calendar />
         </div>
       </div>
-
+      </div>
       <div className={style.carruselContainer}>
         <h2 className={style.title}>Gallery</h2>
         <div>
@@ -24,8 +26,13 @@ const Detail = () => {
         </div>
       </div>
 
-      <hr />
+      <div className={style.reseñasContainer}>
       <h2 className={style.title}>Review</h2>
+
+      </div>
+
+   
+      <Footer />
     </div>
   );
 };
