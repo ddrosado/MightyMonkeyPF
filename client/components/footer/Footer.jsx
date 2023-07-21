c331e2f0c70e513a72ced3630cd3d554e64f6ef4
LucasFigueroa5 <lucasfigueroab@gmail.com>
2023-07-21 20:17:26 -0300

    footer icons ready peerrrra

--- client/components/footer/Footer.jsx
+++ client/components/footer/Footer.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import logo from '../../assets/images/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookF, faInstagram, faSquareFacebook, faSquareInstagram, faSquareTwitter, faTwitter} from '@fortawesome/free-brands-svg-icons'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
@@ -12,9 +13,15 @@ const Footer = () => {
       <p className={style.copyright}>2023 mighty monky club. Todos los derechos reservados</p>
       <p className={style.aboutDevs}>Our Developers</p>
       <div className={style.socialIcons}>
+        <Link href='https://instagram.com/' target='blank'>
         <FontAwesomeIcon className={style.socialIcon} icon={faSquareInstagram}/>
+        </Link>
+        <Link href='https://twitter.com/' target='blank'>
         <FontAwesomeIcon className={style.socialIcon} icon={faSquareTwitter}/>
+        </Link>
+        <Link href='https://facebook.com/' target='blank'>
         <FontAwesomeIcon className={style.socialIcon}  icon={faSquareFacebook} />
+        </Link>
       </div>
     </div>
   )
