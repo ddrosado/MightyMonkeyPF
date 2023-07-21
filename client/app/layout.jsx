7a8886bf24d3fb36926c350d1a92220c749ee7b8
EnzoMarinich <marinichenzo@gmail.com>
2023-07-21 18:02:33 -0300

    delet nav y footer login

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,7 +1,9 @@
+"use client"
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Navbar } from "../components/navBar/Navbar";
 import Footer from "../components/footer/Footer";
+import {usePathname} from "next/navigation"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,13 +14,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 
+  const  path = usePathname()
 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        {path !== "/login"? <Navbar /> : null }
         {children}
-        <Footer />
+        {path !== "/login"? <Footer /> : null }
       </body>
     </html>
   );
