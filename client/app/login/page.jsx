ce723519336264d40da2ea528b2b8b78ac4af80d
LucasFigueroa5 <lucasfigueroab@gmail.com>
2023-07-21 19:52:32 -0300

    codigo acomodado

--- client/app/login/page.jsx
+++ client/app/login/page.jsx
@@ -1,33 +1,33 @@
-'use client'
+"use client";
 import styles from "./login.module.css";
-import validation from "../login/validation"
+import validation from "../login/validation";
 import { useState } from "react";
 import Link from "next/link";
 
 export default function Login() {
-
   const [userData, setUserData] = useState({
-    username: '',
-    password: ''
+    username: "",
+    password: "",
   });
-  
+
   const [errors, setErrors] = useState({});
   // const [isValid, setIsValid] = useState(false);
-  
+
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     setUserData({
       ...userData,
-      [name]: value
+      [name]: value,
     });
-    setErrors(validation({
-      ...userData,
-      [name]: value
-    }));
+    setErrors(
+      validation({
+        ...userData,
+        [name]: value,
+      })
+    );
   };
-  
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const validationErrors = validation(userData);
@@ -36,86 +36,81 @@ export default function Login() {
       // se submitea
     }
     setUserData({
-      username: '',
-      password: ''
+      username: "",
+      password: "",
     });
   };
-  
 
-  return  (
+  return (
     <div className={styles.loginScreen}>
       <div className={styles.loginTrapezoid}>
-      <div className={styles.loginContainer}>
-        <form className={styles.loginForm} onSubmit={handleSubmit}>
-          <div className={styles.loginFormGroup}>
-            <label htmlFor="username">Email</label>
-            <input
-              type="email"
-              id="username"
-              name="username"
-              placeholder="youremail@mail.com"
-              value={userData.username}
-              onChange={handleChange} 
-              class="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-            {errors.username && <p className={styles.loginError}>{errors.username}</p>}
-          </div>
-          
-          
-
-          <div className={styles.loginFormGroup}>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              value={userData.password}
-              onChange={handleChange} 
-              class="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-            {errors.password && <p className={styles.loginError}>{errors.password}</p>}
-          </div>
-          <div className={styles.loginButtonContainer}>
-          <button 
-          type="submit" 
-          class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-            Sign In
-            </button>
+        <div className={styles.loginContainer}>
+          <form className={styles.loginForm} onSubmit={handleSubmit}>
+            <div className={styles.loginFormGroup}>
+              <label htmlFor="username">Email</label>
+              <input
+                type="email"
+                id="username"
+                name="username"
+                placeholder="youremail@mail.com"
+                value={userData.username}
+                onChange={handleChange}
+                class="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+              {errors.username && (
+                <p className={styles.loginError}>{errors.username}</p>
+              )}
             </div>
-        </form>
 
-        <button
-    class="bg-white px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
-    <img 
-    class="w-6 h-6" 
-    src="https://www.svgrepo.com/show/475656/google-color.svg" 
-    loading="lazy" 
-    alt="google logo"/>
-    <span>Sign in with Google</span>
-</button>      
+            <div className={styles.loginFormGroup}>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                value={userData.password}
+                onChange={handleChange}
+                class="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+              {errors.password && (
+                <p className={styles.loginError}>{errors.password}</p>
+              )}
+            </div>
+            <div className={styles.loginButtonContainer}>
+              <button
+                type="submit"
+                class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
 
-        <div 
-        className={styles.forgotPass}>
-          <a href="#">
-            Forgot your password?
+          <button class="bg-white px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
+            <img
+              class="w-6 h-6"
+              src="https://www.svgrepo.com/show/475656/google-color.svg"
+              loading="lazy"
+              alt="google logo"
+            />
+            <span>Sign in with Google</span>
+          </button>
+
+          <div className={styles.forgotPass}>
+            <a href="#">Forgot your password?</a>
+          </div>
+          <div className={styles.createAccount}>
+            <a href="#">
+              Don't have an account?{" "}
+              <span className={styles.signUp}>
+                <u>Sign Up!</u>
+              </span>
             </a>
+            <Link href="/signUp">Link Text</Link>
+          </div>
+          <div className={styles.loginGoogle}></div>
         </div>
-        <div 
-        className={styles.createAccount}>
-          <a 
-          href="#">
-            Don't have an account? <span className={styles.signUp}><u>Sign Up!</u></span>
-            </a>
-            <Link href="/signUp">
-            Link Text
-          </Link>
-        </div>
-        <div 
-        className={styles.loginGoogle}>
-        
-        </div>
-      </div>
       </div>
     </div>
   );
--- client/components/card/Card.jsx
+++ client/components/card/Card.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import styles from "./Card.module.css";
 import Link from "next/link";
 
-
-const Card = ({image}) => {
-
+const Card = ({ image }) => {
   return (
-      <Link href='/detail'>
-    <div className={styles.card} style={{backgroundImage: `url(${image})`}}>
-      <div className={styles.cardDetails}>
-        {/* <div className={styles.sportName}>
+    <Link href="/detail">
+      <div className={styles.card} style={{ backgroundImage: `url(${image})` }}>
+        <div className={styles.cardDetails}>
+          {/* <div className={styles.sportName}>
           <h1>{name}</h1>
         </div> */}
+        </div>
+        <button className={styles.cardButton}>Take a turn</button>
       </div>
-      <button className={styles.cardButton}>Take a turn</button>
-    </div>
-      </Link>
+    </Link>
   );
+
+  
 };
 
 export default Card;
