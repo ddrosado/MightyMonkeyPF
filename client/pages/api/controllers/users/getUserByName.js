c1dbc0310becbc93ffb065641c543dd92beebac4
matinic <117241248+matinic@users.noreply.github.com>
2023-07-21 13:09:35 -0300

    startwith

--- client/pages/api/controllers/users/getUserByName.js
+++ client/pages/api/controllers/users/getUserByName.js
@@ -1,10 +1,12 @@
+const { Op } = require('sequelize');
 const { db } = require('../../db')
 db.sequelize.sync()
+
 const {User} = db;
 
 module.exports = async(name)=>{
     const user = await User.findOne({
-        where:{name: name}
+        where:{name:{[Op.startsWith]:name}}
     })
     if(!user) throw Error('User not found')
     return user
