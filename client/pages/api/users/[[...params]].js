23dad17e461471136b35729a5a6a05177db30941
Jhonatan Bolivar <jhonatanbolivar14@gmail.com>
2023-07-21 13:41:01 -0500

    bookings get y post

--- client/pages/api/users/[[...params]].js
+++ client/pages/api/users/[[...params]].js
@@ -1,9 +1,6 @@
 const getUserById = require('../controllers/users/getUserById')
 const getUserByName = require('../controllers/users/getUserByName')
 
-const { db } = require('../db')
-const User = db.User
-
 export default async(req, res) => {
     try {
         const { params } = req.query
