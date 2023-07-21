9635cd711523cd1b61087890845803ba84087739
matinic <117241248+matinic@users.noreply.github.com>
2023-07-21 11:03:28 -0300

    prueba

---
+++ client/pages/api/bookings/index.js
@@ -0,0 +1,23 @@
+const getBookings = require('../../controllers/bookings/getBookings')
+const postBookings = require('../../controllers/bookings/postBookings')
+
+export default async(req, res) => {
+    switch (req.method) {
+        case 'GET':
+            try {
+                const allBookings = await getBookings()
+                return res.status(200).json(req.query)
+            } catch (error) {
+                return res.status(400).json({error: error.message})
+            }
+        case 'POST':
+            try {
+                const newBooking = postBookings(req.body)
+                return res.status(200).json(newBooking)
+            } catch (error) {
+                return res.status(400).json({error: error.message})
+            }
+        default:
+            break;
+    }
+}
