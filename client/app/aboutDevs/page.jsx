c62c21084594a80739295e6b7eb81c460ace1189
LucasFigueroa5 <lucasfigueroab@gmail.com>
2023-07-22 18:48:31 -0300

    devs

--- client/app/aboutDevs/page.jsx
+++ client/app/aboutDevs/page.jsx
@@ -86,38 +86,3 @@ export default function page() {
     </div>
   );
 }
-import React from "react";
-import style from "./page.module.css";
-import facu from "../../assets/images/devs/facuDev.png";
-import denise from "../../assets/images/devs/deniseDev.png";
-import enzo from "../../assets/images/devs/enzoDev.png";
-import lucas from "../../assets/images/devs/lucasDev.png";
-import gonza from '../../assets/images/devs/gonzaDev.png'
-import Image from "next/image";
-import {
-  faLinkedin,
-  faSquareFacebook,
-  faSquareGithub,
-  faSquareInstagram,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-export default function page() {
-  let devs = [facu, denise, enzo, lucas, gonza];
-  return (
-    <div className={style.devsContainer}>
-      <div className={style.devsGrid}>
-        {devs.map((dev) => (
-          <div className={style.devInfo}>
-            <Image className={style.imageDev} src={dev} alt="#" />
-            <div className={style.infoIcons}>
-              <FontAwesomeIcon className={style.iconDev} icon={faSquareInstagram} />
-              <FontAwesomeIcon className={style.iconDev} icon={faLinkedin} />
-              <FontAwesomeIcon className={style.iconDev} icon={faSquareGithub} />
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
