72f62afb23f1b983d7cc2a03b1589d69f8904bb9
LucasFigueroa5 <lucasfigueroab@gmail.com>
2023-07-21 22:03:08 -0300

    about devs

--- client/app/aboutUsDevs/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div>aboutUsDevs, facu
-    <h1>hola</h1>
-    </div>
-  )
-}
--- client/components/footer/Footer.jsx
+++ client/components/footer/Footer.jsx
@@ -1,30 +1,50 @@
-import React from 'react'
-import style from './Footer.module.css'
-import Image from 'next/image'
-import logo from '../../assets/images/logo.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookF, faInstagram, faSquareFacebook, faSquareInstagram, faSquareTwitter, faTwitter} from '@fortawesome/free-brands-svg-icons'
-import Link from 'next/link'
+import React from "react";
+import style from "./Footer.module.css";
+import Image from "next/image";
+import logo from "../../assets/images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faSquareFacebook,
+  faSquareInstagram,
+  faSquareTwitter,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className={style.footerContainer}>
-      <Image src={logo} alt='logos' className={style.logo} />
-      <p className={style.copyright}>2023 mighty monky club. Todos los derechos reservados</p>
-      <p className={style.aboutDevs}>Our Developers</p>
+      <Image src={logo} alt="logos" className={style.logo} />
+      <p className={style.copyright}>
+        2023 mighty monky club. Todos los derechos reservados
+      </p>
+      <Link href='/aboutDevs'>
+        <p className={style.aboutDevs}>Our Developers</p>
+      </Link>
       <div className={style.socialIcons}>
-        <Link href='https://instagram.com/' target='blank'>
-        <FontAwesomeIcon className={style.socialIcon} icon={faSquareInstagram}/>
+        <Link href="https://instagram.com/" target="blank">
+          <FontAwesomeIcon
+            className={style.socialIcon}
+            icon={faSquareInstagram}
+          />
         </Link>
-        <Link href='https://twitter.com/' target='blank'>
-        <FontAwesomeIcon className={style.socialIcon} icon={faSquareTwitter}/>
+        <Link href="https://twitter.com/" target="blank">
+          <FontAwesomeIcon
+            className={style.socialIcon}
+            icon={faSquareTwitter}
+          />
         </Link>
-        <Link href='https://facebook.com/' target='blank'>
-        <FontAwesomeIcon className={style.socialIcon}  icon={faSquareFacebook} />
+        <Link href="https://facebook.com/" target="blank">
+          <FontAwesomeIcon
+            className={style.socialIcon}
+            icon={faSquareFacebook}
+          />
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
