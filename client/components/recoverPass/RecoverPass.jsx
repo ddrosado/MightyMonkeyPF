7b5f864790f6e6e6c80e587ccab7b38454692421
Denise <deniselucianarosado@gmail.com>
2023-07-22 11:30:36 -0300

    zzzzz

--- client/components/recoverPass/RecoverPass.jsx
+++ client/components/recoverPass/RecoverPass.jsx
@@ -1,4 +1,4 @@
-import styles from "../../app/login.module.css"
+import styles from "./Recover.module.css"
 import BackArrow from "../goBack/BackArrow";
 
 const Recover = (props) => {
@@ -7,7 +7,32 @@ const Recover = (props) => {
     <div>
       <BackArrow 
       changeComponent={props.changeComponent}/>
-      <h1>recover</h1>
+      <div className={styles.recoverContainer}>
+
+
+        <div className={styles.recoverEmailContainer}>
+        <p>
+          Email Adress
+        </p>
+        </div>
+
+      <div className={styles.recoverInputContainer}>
+        <input
+      type="email"
+      id="recovermail"
+      name="recovermail"
+      placeholder="youremail@mail.com"
+      value=""
+      class="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    />
+    </div>
+
+        <button 
+  type="submit" 
+  className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-slate-300 rounded shadow">
+    Send Reset Code
+    </button>
+        </div>
       </div>
   )
   };
