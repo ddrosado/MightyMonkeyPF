8435e32dd96cc175f884c1939b988c981ae96066
Denise <deniselucianarosado@gmail.com>
2023-07-23 19:14:38 -0300

    redux mas organizado

--- client/redux/features/sportsSlice.js
+++ client/redux/features/sportsSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getSports } from '../actions/sportsActions';
 
 const initialState = {
   sports: 0,
+  error: "",
 };
 
 const sportsSlice = createSlice({
@@ -12,7 +14,14 @@ const sportsSlice = createSlice({
       state.sports += 1;
     },
   },
-});
+  extraReducers: (builder) => {
+    builder
+      .addCase(getSports.fulfilled, (state, action) => { 
+        state.error = "";
+        state.sports = action.payload;
+      })
+    },
+  });
 
 export const { increment } = sportsSlice.actions;
 export default sportsSlice.reducer;
