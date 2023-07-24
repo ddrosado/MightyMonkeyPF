dd1e05ca0747b5b9cf81981cf7a2fb361ca50a84
Denise <deniselucianarosado@gmail.com>
2023-07-23 22:50:51 -0300

    ola

---
+++ client/redux/actions/userActions.js
@@ -0,0 +1,16 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'http://localhost:3000';
+
+export const getUsers = createAsyncThunk(
+    'sports/getSports',
+    async () => {
+      try {
+        const response = await axios.get(`${url}/api/users`);
+        return response.data;
+      } catch (error) {
+        throw error.response.data.msg;
+      }
+    }
+  );
