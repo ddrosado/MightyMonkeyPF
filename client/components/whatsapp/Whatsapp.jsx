6545624addaaa0c6571d1eddf0b18278709987de
LucasFigueroa5 <lucasfigueroab@gmail.com>
2023-07-23 23:33:02 -0300

    wapp

--- client/components/whatsapp/Whatsapp.jsx
+++ client/components/whatsapp/Whatsapp.jsx
@@ -4,8 +4,19 @@ import React from 'react'
 import style from './Whatsapp.module.css'
 
 const Whatsapp = () => {
+  const phoneNumber = '+541165788585';
+  const message = 'Hola Facundo pedacito de nazi';
+
+  const handleWhatsappClick = () => {
+
+    const encodedMessage = encodeURIComponent(message);
+
+    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+    window.open(whatsappLink, '_blank');
+  };
   return (
-    <div className={style.whatsappIcon}>
+    <div onClick={handleWhatsappClick} className={style.whatsappIcon}>
         <FontAwesomeIcon icon={faSquareWhatsapp} style={{color: "#46d250",}} />
     </div>
   )
