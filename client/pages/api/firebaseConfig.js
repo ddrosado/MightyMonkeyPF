36c3b1e149e3088d3e6409b6cedbf69596bd6371
Denise <deniselucianarosado@gmail.com>
2023-07-25 19:50:20 -0300

    pop up google

--- client/pages/api/firebaseConfig.js
+++ client/pages/api/firebaseConfig.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBeQSChvNUCfjiwKPRHey5Lf5ZcQEqi2IA",
   authDomain: "mightymonkeyclub-861ae.firebaseapp.com",
@@ -16,6 +11,7 @@ const firebaseConfig = {
   measurementId: "G-XE07K6MKRR"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export default app;
