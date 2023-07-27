f9bfe764337cf93877b21a783b9da909c9af24c1
Jhonatan Bolivar <jhonatanbolivar14@gmail.com>
2023-07-27 15:55:40 -0500

    mercado pago

--- client/pages/api/controllers/payment/payment.js
+++ client/pages/api/controllers/payment/payment.js
@@ -1,32 +1,43 @@
 const mercadopago = require('mercadopago');
 
 
-module.exports = async(req, res) => {
+module.exports = async(data, type) => {
     mercadopago.configure({
         access_token: 'TEST-872443940722018-072322-5276e0527cfd7c712ab71c09327023e0-1431922934'
     });
-    const result = await mercadopago.preferences.create({
-        items: [
-            {
-                title: 'Reserva cancha',
-                unit_price: 15000,
-                currency_id: 'COP',
-                quantity: 1
-            },
-            {
-                title: 'Reserva cancha',
-                unit_price: 15000,
-                currency_id: 'COP',
-                quantity: 1
-            }
-        ],
-        back_urls: {
-            failure: 'https://localhost:3000/api/failure',
-            pending: 'https://localhost:3000/api/pending',
-            success: 'https://localhost:3000/api/success'
-        },
-        notification_url: 'https://f50b-179-1-48-61.ngrok.io/api/webHookPay'
-    })
-    console.log(result);
-    return result
+    console.log('this is type', type);
+    switch (type) {
+        case 'buys': {
+            const { items } = data
+            console.log('esto es req',items);
+            const result = await mercadopago.preferences.create({
+                items: items,
+                back_urls: {
+                    failure: 'https://localhost:3000/api/failure',
+                    pending: 'https://localhost:3000/api/pending',
+                    success: 'https://localhost:3000/api/success'
+                },
+                notification_url: 'https://0a11-179-1-48-61.ngrok.io/api/webHookPay'
+            })
+            return result
+        }
+        case 'subscriptions': {
+            const { email, reason, price } = data
+            const result = await mercadopago.preapproval.create({
+                payer_email: email, //"test_user_1751930390@testuser.com",
+                reason: reason,
+                auto_recurring: {
+                    frequency: 1,
+                    frequency_type: "months",
+                    transaction_amount: price,
+                    currency_id: "COP"
+                },
+                back_url: "https://6457-179-1-48-61.ngrok.io/gracias",
+            })
+            console.log(result);
+            return result
+        }
+        default:
+            throw new Error('Invalid url parameters');
+    }
 }
--- client/pages/api/webHookPay/index.js
+++ client/pages/api/webHookPay/index.js
@@ -1,14 +1,23 @@
 import mercadopago from "mercadopago";
-
+const postBookings = require('../controllers/bookings/postBookings')
 
 export default async(req, res) => {
     try {
-        console.log(req.query);
+        console.log('QUERY',req.query);
+        console.log('BODY',req.body);
+        console.log('METHOD',req.method);
         const payment = req.query
         if(payment.type === 'payment') {
-            const data = await mercadopago.payment.findById(payment['data.id']);
-            console.log(data);
-            res.status(200).json(data);
+            const newBookings = [];
+            const bookingsItems = (await mercadopago.payment.findById(payment['data.id'])).response.additional_info.items;
+            console.log('items ?', bookingsItems);
+            bookingsItems.forEach(async(item) => {
+                const [ date, schedule, duration ] = item.description.split(' ');
+                const info = { date, schedule, duration: Number(duration)}
+                const newBooking = await postBookings(info)
+                newBookings.push(newBooking)
+            });
+            res.status(200).json(newBookings);
         }
     } catch (error) {
         
