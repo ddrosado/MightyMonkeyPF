bd73ccec403030ca4869a962e13e265e65071b7a
matinic <117241248+matinic@users.noreply.github.com>
2023-07-28 21:43:34 -0300

    crud sport 2

--- client/pages/api/controllers/sport/updateSport.js
+++ client/pages/api/controllers/sport/updateSport.js
@@ -2,13 +2,13 @@ import { db } from "../../db";
 const { Sport,Court } = db;
 
 export default async (sportInfo) => {
-    const sport = await Sport.update(sportInfo, {
-      where: { id: sportInfo.id },
-    });
-    if(!sport) throw Error('unnfound sport')
-    const updatedSport = await Sport.findByPk(
-      sportInfo.id,
-      {
+  const sport = await Sport.update(sportInfo, {
+    where: { id: sportInfo.id },
+  });
+  if (!sport) return sport;
+  const updatedSport = await Sport.findByPk(
+    sportInfo.id,
+    {
         attributes: {
           exclude: [
             'createdAt',
