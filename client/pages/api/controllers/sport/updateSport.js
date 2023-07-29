f41dfb15a34ebf94b7521c752ef711f76100e545
matinic <117241248+matinic@users.noreply.github.com>
2023-07-28 21:19:05 -0300

    sport crud

--- client/pages/api/controllers/sport/updateSport.js
+++ client/pages/api/controllers/sport/updateSport.js
@@ -2,31 +2,32 @@ import { db } from "../../db";
 const { Sport,Court } = db;
 
 export default async (sportInfo) => {
-  const sport = Sport.update(sportInfo, {
-    where: { id: sportInfo.id },
-  });
-  if (!sport) return sport;
-  const updatedSport = Sport.findByPk(
-    sportInfo.id,
-    {
+    const sport = await Sport.update(sportInfo, {
+      where: { id: sportInfo.id },
+    });
+    if(!sport) throw Error('unnfound sport')
+    const updatedSport = await Sport.findByPk(
+      sportInfo.id,
+      {
         attributes: {
-            exclude: [
-                'createdAt',
-                'updatedAt'
-            ]
+          exclude: [
+            'createdAt',
+            'updatedAt'
+          ]
         },
         include:{
-            model: Court,
-            as: 'court',
-            attributes:{
-                exclude: [
-                    'createdAt',
-                    'updatedAt',
-                    'sportId'
-                ]
-            }
+          model: Court,
+          as: 'court',
+          attributes:{
+            exclude: [
+              'createdAt',
+              'updatedAt',
+              'sportId'
+            ]
+          }
         }
-    }
-  );
-  return updatedSport;
+      }
+      );
+    if(!updatedSport) throw Error('unnfound Sport')
+    return updatedSport;
 };
--- client/pages/api/sport/index.js
+++ client/pages/api/sport/index.js
@@ -1,24 +1,35 @@
 import updateSport from "../controllers/sport/updateSport";
+import deleteSport from "../controllers/sport/deleteSport";
 
-const postSport = require("../controllers/sport/postSports")
-const getAllSports = require("../controllers/sport/getSports")
+const postSport = require("../controllers/sport/postSports");
+const getAllSports = require("../controllers/sport/getSports");
 
-export default async(req, res) => {
-    try {
-        switch (req.method) {
-            case 'GET':
-                const allSports = await getAllSports();
-                return res.status(200).json(allSports)
-            case 'POST':
-                const newSport = await postSport(req.body)
-                return res.status(200).json(newSport)
-            case 'PUT':
-                const sport = await updateSport(req.body)
-                return res.status(200).json(sport)
-            default:
-                break;
-        }    
-    } catch (error) {
-        return res.status(200).json(error.message)
+export default async (req, res) => {
+  const { method, body } = req;
+  try {
+    if (method === "GET") {
+      const sport = await getAllSports();
+      return res.status(200).json(sport);
     }
-}
+    if (method === "POST") {
+      const { name, image, description } = body;
+      if (!name || !image || !description) throw Error("missing data");
+      const sport = await postSport(body);
+      return res.status(200).json(sport);
+    }
+    if (method === "PUT") {
+      const { id } = body;
+      if (!id) throw Error("missing id");
+      const sport = await updateSport(body);
+      return res.status(200).json(sport);
+    }
+    if (method === "DELETE") {
+      const { id } = body;
+      if (!id) throw Error("missing id");
+      const sport = await deleteSport(id);
+      return res.status(200).json(sport);
+    }
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
