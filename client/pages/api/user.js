f7be746ee07c40ece63addd026ee60f4fabee487
matinic <117241248+matinic@users.noreply.github.com>
2023-07-29 14:10:08 -0300

    user fetch autentication

--- client/pages/api/user.js
+++ client/pages/api/user.js
@@ -1,11 +1,8 @@
-import { withSession } from "./middleware";
+import { withSession } from "./controllers/sessionAuth/middleware";
 
 export default withSession(async (req, res) => {
     const user = req.session.get("user");
-  
     if (user) {
-      // in a real world application you might read the user id from the session and then do a database request
-      // to get more information on the user if needed
       res.json({
         isLoggedIn: true,
         ...user,
