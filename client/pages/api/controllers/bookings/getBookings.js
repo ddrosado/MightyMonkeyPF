2400c53f6b68fc9e6c64ee561b609ca6e0b3049a
Jhonatan Bolivar <jhonatanbolivar14@gmail.com>
2023-07-30 15:54:54 -0500

    relaciones booking actualizadas

--- client/pages/api/controllers/bookings/getBookings.js
+++ client/pages/api/controllers/bookings/getBookings.js
@@ -1,8 +1,31 @@
 const { db } = require('../../db')
 db.sequelize.sync()
-const Booking = db.Booking
+const { Booking, User, Court } = db
 
 module.exports = async() => {
-    const bookings = await Booking.findAll()
+    const bookings = await Booking.findAll({
+        attributes: [
+            'id', 'date', 'schedule', 'duration'
+        ],
+        include: [
+            {
+                model: User,
+                as: 'user',
+                attributes: [
+                    'name',
+                    'email'
+                ]
+            },
+            {
+                model: Court,
+                as: 'court',
+                attributes: [
+                    'name',
+                    'description'
+                ]
+            }
+        ]
+    })
+
     return bookings;
 }
