98f03fe6ca4a5b0b71ece649003996c7417f299c
Facundo nuno <103080547+FacundoNuno@users.noreply.github.com>
2023-07-29 23:17:31 -0300

    404notfound

---
+++ client/app/404NotFound/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import imgage from "../../assets/images/logo404.png";
+import Image from "next/image";
+import styles from "./page.module.css";
+
+
+
+
+
+const notFound = () => {
+
+return (
+    <div className={styles.container}>
+        <div>
+            <Image className={styles.imgage}src={imgage} alt="404" />
+            <h1>404 Not Found</h1>
+        </div>
+    </div>
+);
+};
+
+export default notFound;
