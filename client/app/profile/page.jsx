7a6d536d1ea613bddb6216369fce8dd443d79e22
Denise <deniselucianarosado@gmail.com>
2023-07-30 21:17:46 -0300

    profile changes

--- client/app/profile/page.jsx
+++ client/app/profile/page.jsx
@@ -6,6 +6,8 @@ import { Turns } from '../../components/turns/Turns'
 import useSWR from "swr";
 import { fetcher } from '../../pages/fetcher';
 import { Unauthorized } from '../unauthorized/page';
+import loading from "../../assets/images/giphy.gif"
+import Image from 'next/image'
 
 const Profile = () => {
 
@@ -27,9 +29,11 @@ const Profile = () => {
         ) : (
           <Unauthorized />
         )
-      ) : (
-        <Unauthorized />
-      )}
+      ) : 
+      <div className={style.loadingMonkeyContainer}>
+        <Image className={style.loading} src={loading} alt="gif" />
+        </div>
+      }
     </>
   );
 };
