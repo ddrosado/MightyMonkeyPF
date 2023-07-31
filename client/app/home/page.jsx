29a5f62cff4957f6bc0974a84175de8ad327a181
Denise <deniselucianarosado@gmail.com>
2023-07-31 14:57:34 -0300

    sign up css

--- client/app/home/page.jsx
+++ client/app/home/page.jsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import Cards from "../../components/cards/Cards";
 import styles from './page.module.css'
-import homeImage from '../../assets/images/mmclub.jpg'
 import Banner from "../../components/banner/Banner";
 
 export default function Home() {
