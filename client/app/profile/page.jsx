6671b0d157632b35b17370058eda52758c1b056d
Denise <deniselucianarosado@gmail.com>
2023-07-30 21:01:04 -0300

    required auth view

--- client/app/profile/page.jsx
+++ client/app/profile/page.jsx
@@ -1,18 +1,37 @@
+'use client'
 import React from 'react'
 import MyProfile from '../../components/profile/Profile'
 import style from "./page.module.css"
 import { Turns } from '../../components/turns/Turns'
+import useSWR from "swr";
+import { fetcher } from '../../pages/fetcher';
+import { Unauthorized } from '../unauthorized/page';
 
 const Profile = () => {
+
+  const { data, error } = useSWR("api/user", fetcher);
+  console.log(data)
+
+
   return (
-    <div className={style.container}>
-      <h1>Personal Dates</h1>
-      <MyProfile/>
-      <h1>My Turns</h1>
-      <Turns/>
-      <Turns/>
-    </div>
-  )
-}
+    <>
+      {data ? (
+        data.isLoggedIn ? (
+          <div className={style.container}>
+            <h1>My Profile</h1>
+            <MyProfile />
+            <h1>My Turns</h1>
+            <Turns />
+            <Turns />
+          </div>
+        ) : (
+          <Unauthorized />
+        )
+      ) : (
+        <Unauthorized />
+      )}
+    </>
+  );
+};
 
 export default Profile
