6c5cd9c38f6d73cbc227808f058217ef2ebbeee5
Jhonatan Bolivar <jhonatanbolivar14@gmail.com>
2023-08-02 11:29:30 -0500

    first commit

---
+++ client/pages/api/controllers/utils/mails.js
@@ -0,0 +1,11 @@
+const nodemailer = require("nodemailer");
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'mightymonkeys25@gmail.com',
+    pass: 'ivioquzngtvhngjq'
+  }
+});
