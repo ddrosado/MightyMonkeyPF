6fd929b4931b19d5af68651f96e7309ba8ab665a
Facundo nuno <103080547+FacundoNuno@users.noreply.github.com>
2023-08-01 22:17:09 -0300

    getBookingCorts

--- client/app/home/page.jsx
+++ client/app/home/page.jsx
@@ -1,6 +1,10 @@
 import Cards from "../../components/cards/Cards";
 import styles from './page.module.css'
+<<<<<<< .merge_file_a00476
 import Banner from "../../components/banner/Banner";
+=======
+// import Banner from "../../components/banner/Banner";
+>>>>>>> .merge_file_a08216
 
 export default function Home() {
   return (
