c33606decccc121fbc2606dcd7c520548641f7ed
matinic <117241248+matinic@users.noreply.github.com>
2023-08-02 11:29:37 -0300

    login/user restriction

--- client/pages/api/controllers/sessionAuth/middleware.js
+++ client/pages/api/controllers/sessionAuth/middleware.js
@@ -6,7 +6,6 @@ export function withSession(handler) {
   return withIronSession(handler, {
     password: SESSION,
     cookieName: 'mightyMonkey',
-    
     // cookieOptions: {
     //   secure: process.env.NODE_ENV === 'production', // Solo enviar cookies en conexiones HTTPS en producción.
     // },
