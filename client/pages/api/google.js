316fc08d7eb24258bca52f90f8a8a66907b0fb8c
Denise <deniselucianarosado@gmail.com>
2023-08-03 17:53:30 -0300

    ...

--- client/pages/api/google.js
+++ client/pages/api/google.js
@@ -1,13 +1,56 @@
-import postUser from "./controllers/users/postUser"
+import userAuth from "./controllers/userAuth/userAuth";
+import postUser from "./controllers/users/postUser";
+import { withSession } from "./controllers/sessionAuth/middleware";
+import { transporter } from './controllers/utils/mails'
+const fs = require('fs')
+const htmlstream = fs.createReadStream("./pages/api/controllers/users/mail/content.html");
 
-export default async (req,res)=>{
-    const {body,method} = req
-    if(method === 'POST'){
-        const data = body.providerData[0]
-        return res.status(200).json({
-            backend:{
-               data
-            }
-        })
+async function handler(req, res) {
+  const { body } = req;
+  const { set, save } = req.session;
+  const data = body.providerData[0];
+  try {
+    const { email, uid: password } = data;
+    const existingUser = await userAuth({ email, password });
+    const userInfo = {
+      name: existingUser.name,
+      surname: existingUser.surname,
+      email: existingUser.email,
+      id: existingUser.id,
+      isAdmin: existingUser.isAdmin,
+      isActive: existingUser.isActive,
+      image: existingUser.image,
+      planId: existingUser.planId,
+    };
+    set("user", userInfo);
+    await save();
+    return res.status(200).json({ session: true });
+  } catch (error) {
+    try{
+        const newUser = {
+          name: data.displayName?.split(" ")[0],
+          surname: data.displayName?.split(" ")[1],
+          image: data.photoURL,
+          email: data.email,
+          telelephone: data.phoneNumber,
+          password: data.uid,
+        };
+        const { name, email, id, isAdmin, image, planId, surname, isActive } = await postUser(
+          newUser
+        );
+        set("user", { name, email, id, planId, isAdmin, image, surname, isActive });
+        await save();
+        await transporter.sendMail({
+            from: '"Mighty Monkeys" <mightymonkeys25@gmail.com>',
+            to: email,
+            subject: "Te damos la bienvenida a Mighty Monkeys",
+            html: htmlstream.on('data', (data) => data.toString()),
+          });
+        return res.status(200).json( {session:true} );
+    }catch(error){
+        return res.status(400).json(error)
     }
+  }
 }
+
+export default withSession(handler);
--- client/pages/api/login.js
+++ client/pages/api/login.js
@@ -6,10 +6,15 @@ async function handler(req, res) {
   const { set, save } = req.session;
   try {
     if (method === "POST") {
+<<<<<<< HEAD
       const { name, surname, email, id, isMember, isAdmin, isActive } = await userAuth(body);
       set("user", { name, surname, email, id, isMember, isAdmin, isActive });
+=======
+      const { name, email, id, isAdmin, image, planId, isActive } = await userAuth(body);
+      set("user", { name, email, id, planId, isAdmin, image, isActive });
+>>>>>>> c5ef15e82621c3b9d2fe0ee605d99caced1b8311
       await save();
-      return res.status(200).json({ session: true });
+      return res.status(200).json({ session: true, isActive });
     }
   } catch (error) {
     return res.status(401).json(error.message);
