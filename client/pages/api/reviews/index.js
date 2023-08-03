da051bd73c15bb3d68a09f424e2d7edc6b893bf2
Facundo nuno <103080547+FacundoNuno@users.noreply.github.com>
2023-08-03 01:24:30 -0300

    reviews

--- client/pages/api/reviews/index.js
+++ client/pages/api/reviews/index.js
@@ -4,16 +4,16 @@ import deleteReview from '../controllers/review/deleteReview'
 export default async(req,res)=>{
     
     try {
-        const {comment,rating,courtId} = req.body;
+        const {comment,rating} = req.body;
         switch (req.method){
             case 'POST':
                 if(comment.lenght < 10) throw Error ('Comment is too short')
-                if(!comment || !rating || !courtId) throw Error ("Missiong Data") 
+                if(!comment || !rating ) throw Error ("Missiong Data") 
                 const review = await postReview(req.body)
                 return res.status(200).json(review)
             case 'GET':
                 const courtReview = await getReview()
-                return res.status(400).json(courtReview)
+                return res.status(200).json(courtReview)
             case 'DELETE':
             const reviewUpdate = await deleteReview(req.body)
             return res.status(200).json(reviewUpdate)
--- client/redux/store.js
+++ client/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sportsReducer from './features/sportsSlice';
 import usersReducer from './features/usersSlice';
+import reviewsReducer from './features/reviewsSlice';
 
 const store = configureStore({
   reducer: {
     sports: sportsReducer,
-    users: usersReducer
+    users: usersReducer,
+    reviews: reviewsReducer,
   },
 });
 
