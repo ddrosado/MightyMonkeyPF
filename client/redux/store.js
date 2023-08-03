77e00f884c910c1ac3fc86b0b693c5e1af8f8f67
Facundo nuno <103080547+FacundoNuno@users.noreply.github.com>
2023-08-03 17:54:50 -0300

    reviews in home

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,26 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sportsReducer from './features/sportsSlice';
 import usersReducer from './features/usersSlice';
-
 import reviewsReducer from './features/reviewsSlice';
-
-<<<<<<< HEAD
-import reviewsReducer from './features/reviewsSlice';
-=======
 import bookingsSlice from './features/bookingsSlice';
-=========
 import reviewsReducer from './features/reviewsSlice';
->>>>>>>>> Temporary merge branch 2
 
 const store = configureStore({
   reducer: {
     sports: sportsReducer,
     users: usersReducer,
-<<<<<<<<< Temporary merge branch 1
-    bookings : bookingsSlice
-=========
+    bookings : bookingsSlice,
     reviews: reviewsReducer,
->>>>>>>>> Temporary merge branch 2
   },
 });
 
