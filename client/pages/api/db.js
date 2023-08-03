a20b96361b10ddecffd99cde12fd31f7b5fcfafd
matinic <117241248+matinic@users.noreply.github.com>
2023-08-03 15:09:10 -0300

    mutate

--- client/pages/api/db.js
+++ client/pages/api/db.js
@@ -38,9 +38,9 @@ const sequelize = new Sequelize( postgres_DATABASE,postgres_USER, postgres_PASSW
    operatorAliases: false,
    logging: false,
    native: false,
-   dialectOptions: {
-      ssl: true, 
-    },
+   // dialectOptions: {
+   //    ssl: true, 
+   //  },
    pool: {
       max: 5,
       min: 0,
