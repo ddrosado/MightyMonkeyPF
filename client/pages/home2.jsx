bef84c889c6bd8595ee78d45cff79900e741d4db
matinic <117241248+matinic@users.noreply.github.com>
2023-08-03 22:37:00 -0300

    put review

--- client/pages/home2.jsx
+++ client/pages/home2.jsx
@@ -2,15 +2,22 @@ import { useRouter } from "next/router";
 import useSWR from "swr";
 import {fetcher} from './api/fetcher'
 
-const logout = async () => {
+
+
+
+const logout = async (props) => {
+  console.log(props)
   const data = await fetch("http://localhost:3000/api/logout", {
     method: "GET",
   });
   const res = await data.json();
   return res;
 };
-export default function Home2() {
 
+
+
+export default function Home2() {
+ 
  const { data, error } = useSWR("api/user", fetcher);
 
   const router = useRouter();
