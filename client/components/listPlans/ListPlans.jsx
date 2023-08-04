47f62d7b7b184318336b4431a3550a8018497051
EnzoMarinich <marinichenzo@gmail.com>
2023-08-04 19:47:27 -0300

    .

--- client/components/listPlans/ListPlans.jsx
+++ client/components/listPlans/ListPlans.jsx
@@ -1,84 +1,28 @@
 "use-client"
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from "react";
-import {useDispatch, useSelector} from "react-redux"
+import {useDispatch} from "react-redux"
 import { getPlans } from '../../redux/actions/plansActions'
-import style from "./ListPlans.module.css"
-import loading from '../../assets/images/giphy.gif'
-import Image from "next/image";
+import { List } from './list/List';
+import { Form } from './form/Form';
+
 
 const ListPlans = () => {
+  
+  const [current, setCurrent] = useState("list")
+  const dispatch = useDispatch()
 
-    const dispatch = useDispatch()
-    const plans = useSelector(state=> state.plans.plans)
-    const colums = ["Plan", "Price", "duration", ""]
 
     useEffect(()=>{
         dispatch(getPlans()) 
     },[])
 
-    
+
 
   return (
-    <div className={`container mx-auto px-4 sm:px-8 ${style.container}`}>
-    <div className="py-8">
-      <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-        <div
-          className={`inline-block shadow rounded-lg overflow-hidden ${style.table}`}
-        >
-          <table className="min-w-full leading-normal">
-            <thead>
-              <tr>
-                {colums.map((col) => {
-                  return (
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                      {col}
-                    </th>
-                  );
-                })}
-              </tr>
-            </thead>
-            <tbody>
-            { plans?.length? 
-              plans.map(({ name, price, duration }) => {
-                return (
-                  <tr key={name}>
-                    <td  className={`px-5 py-5 bg-white ${style.name}`}>
-                      <strong>
-                        {name}
-                      </strong>
-                    </td>
-                    <td className={`px-5 py-5 bg-white ${style.name}`}>
-                      <p className="font-medium text-gray-800 dark:text-white">
-                        ${price}
-                      </p>
-                    </td>
-                    <td className={`px-5 py-5 bg-white ${style.name}`}>
-                      <span className={style.duration}>
-                        {duration}<span style={{fontSize:"0.8em"}}>mes</span>
-                      </span>
-                    </td>
-                    <td className={`px-5 py-5 bg-white ${style.edit}`}>
-                      <button className=" bg-red-500 hover:bg-red-400 text-white font-semibold py-2 px-4 rounded-full">
-                        edit
-                      </button>
-                    </td>
-                  </tr>
-                )
-              }) : <Image className={style.loading} src={loading} alt="gif" />} 
-            </tbody>
-          </table>
-          <div className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-center">
-            <div className="inline-flex mt-2 xs:mt-0">
-              <button  className="text-sm bg-violet-600 hover:bg-violet-500 text-white font-semibold py-2 px-4">
-                Add Plan
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+    <>
+    {current == "list"?  <List setCurrent={setCurrent}/> : current == "create"? <Form setCurrent={setCurrent}/> : <Form setCurrent={setCurrent} id={current} />}
+    </>
   )
 }
 
--- client/redux/actions/plansActions.js
+++ client/redux/actions/plansActions.js
@@ -15,3 +15,26 @@ export const getPlans = createAsyncThunk(
     }
 )
 
+export const postPlans = createAsyncThunk(
+    'plans/postPlans',
+    async (obj) =>{
+        try {
+            const resp = await axios.post(url+"/api/plans" , obj)
+            return resp.data
+        } catch (error) {
+            throw new Error(error.response.data.msg)
+        }
+    }
+)
+
+export const putPlans = createAsyncThunk(
+    'plans/putPlans',
+    async (obj) =>{
+        try {
+            const resp = await axios.put(url+"/api/plans" , obj)
+            return resp.data
+        } catch (error) {
+            throw new Error(error.response.data.msg)
+        }
+    }
+)
