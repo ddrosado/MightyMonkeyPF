3cdd49a9fd6a63cc1c269c1093c1e1253793db1d
LucasFigueroa5 <lucasfigueroab@gmail.com>
2023-08-08 00:00:57 -0300

    explotao

--- client/pages/api/controllers/bookings/postBookings.js
+++ client/pages/api/controllers/bookings/postBookings.js
@@ -23,6 +23,42 @@ module.exports = async (info) => {
     };
   
     const newBooking = await Booking.create(bookingInfo);
+    // console.log(info)
+    // await transporter.sendMail({
+    //     from: '"Mighty Monkeys" <mightymonkeys25@gmail.com>',
+    //     to: user.email,
+    //     subject: "Reserva cancha Mighty Monkeys",
+    //     text: 'Tu reserva',
+    //     html: `<head>
+    //     <style>
+    //         #button {
+    //             background-color: black;
+    //             color: #fff;
+    //             text-decoration: none;
+    //             padding: 12px 20px;
+    //             border: none;
+    //             border-radius: 4px;
+    //             font-size: 16px;
+    //             cursor: pointer;
+    //         }
+    
+    //         #button:hover {
+    //             background-color: rgb(255, 218, 96);
+    //             color: black;
+    //         }
+    //     </style>
+    // </head>
+    //     <img src="https://i.ibb.co/B4B4NLV/logo.png" width='160px' heigth='150px' id="image" alt="">
+    //     <h1>Tu reserva</h1>
+    //     <h2>Date: ${date}</h2>
+    //     <h2>Hour: ${info.hour[0]}hrs</h2>
+    //     <h2>Court: ${courtId.name}</h2>
+    //     <h2>Payment state: ${info.payment? info.payment.type: 'no pay'}</h2>
+    
+    //     <a id="button" href='https://mighty-monkey-pf.vercel.app/home'>
+    //         Ir a la página
+    //     </a>`,
+    //   });
   
     return newBooking;
   };
