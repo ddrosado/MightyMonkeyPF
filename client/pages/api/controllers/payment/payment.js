4e3767ef44af932d444f7c5b0a44cce45f14cfcf
Jhonatan Bolivar <jhonatanbolivar14@gmail.com>
2023-08-08 14:37:02 -0500

    urls para deploy

--- client/pages/api/controllers/payment/payment.js
+++ client/pages/api/controllers/payment/payment.js
@@ -5,7 +5,7 @@ const { User, Court, Plan, Booking } = db
 
 module.exports = async(data) => {
     mercadopago.configure({
-        access_token: 'TEST-5280417047762022-072715-6cdc99477060d48978bc1cf779776e2e-1431922934'
+        access_token: 'TEST-3840529657724541-080815-fba912c6e91d677be2f3b4e4aa59e138-1445796506'
     });
     switch (data.type) {
         case 'bookings': {
