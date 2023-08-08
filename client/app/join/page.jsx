f6021e718d0316a715d5fc3b250d97c72801f985
LucasFigueroa5 <lucasfigueroab@gmail.com>
2023-08-08 12:32:15 -0300

    sda

--- client/app/join/page.jsx
+++ client/app/join/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import style from './page.module.css'
-// import Membership from '../../components/membership/Membership'
+import Membership from '../../components/membership/Membership'
+import PricingPlans from '../../components/pricingPlans/pricingPlans'
 
 const page = () => {
   return (
     <div className={style.joinContainer}>
-      {/* <Membership/> */}
+      <Membership/>
+    
     </div>
   )
 }
