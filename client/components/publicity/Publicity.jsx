4601e8081723f181b96a1eae7fa2c5566710c1a8
LucasFigueroa5 <lucasfigueroab@gmail.com>
2023-08-09 00:24:53 -0300

    home and cards

--- client/components/publicity/Publicity.jsx
+++ client/components/publicity/Publicity.jsx
@@ -8,20 +8,17 @@ import logo from "../../assets/images/logopu.png";
 const Publicity = () => {
   return (
     <div className={style.publicityContainer}>
-      <div className={style.textContainer}>
-        <h1>
-          Welcome to<span>MIGHTY MONKEY CLUB.</span>
-        </h1>
-        <h3>
-          Let's take a
-          <Link className={style.reserve} href={"/sports"}>
-            <span >reservation now!</span>
-          </Link>
-        </h3>
-      </div>
-      <div className={style.separator}></div>
       <div className={style.logoContainer}>
         <Image className={style.logo} src={logo} alt="#" />
+      </div>
+      <div className={style.separator}></div>
+      <div className={style.btnsContainer}>
+        <Link className={style.reserve} href={"/join"}>
+          <button className={style.btnJoin}> Join us!</button>
+        </Link>
+        <Link className={style.reserve} href={"/sports"}>
+          <button className={style.btnReserve}> Reserve a court</button>
+        </Link>
       </div>
     </div>
   );
