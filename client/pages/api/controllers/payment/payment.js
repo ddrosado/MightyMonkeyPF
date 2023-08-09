102a3777ea73b3b00b487d98081b36bac81d01ff
Jhonatan Bolivar <jhonatanbolivar14@gmail.com>
2023-08-09 17:21:37 -0500

    mercadopago pruebas

--- client/pages/api/controllers/payment/payment.js
+++ client/pages/api/controllers/payment/payment.js
@@ -36,6 +36,7 @@ module.exports = async(data) => {
             return result
         }
         case 'subscriptions': {
+            console.log('llego al back');
             const { userId, planId } = data
             const plan = await Plan.findOne({ where: { id: planId } })
             const user = await User.findOne({ where: { id: userId } })
