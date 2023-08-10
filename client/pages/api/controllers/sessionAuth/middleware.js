e5e2f875ddf7736384034410f42b796c4aacb836
Denise <deniselucianarosado@gmail.com>
2023-08-09 23:16:37 -0300

    .

--- client/pages/api/controllers/sessionAuth/middleware.js
+++ client/pages/api/controllers/sessionAuth/middleware.js
@@ -6,9 +6,9 @@ export function withSession(handler) {
   return withIronSession(handler, {
     password: SESSION,
     cookieName: 'mightyMonkey',
-    cookieOptions: {
-      // secure: process.env.NODE_ENV === 'production', // Solo enviar cookies en conexiones HTTPS en producción.
-    },
+    // cookieOptions: {
+    //   secure: process.env.NODE_ENV === 'production', // Solo enviar cookies en conexiones HTTPS en producción.
+    // },
   });
 }
 
