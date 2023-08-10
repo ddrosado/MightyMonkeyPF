435dedfdba688cccca62f789027d4311d3346a68
Denise <deniselucianarosado@gmail.com>
2023-08-10 07:14:54 -0300

    m

--- client/pages/services/api.js
+++ client/pages/services/api.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 const apiMP = axios.create({
   baseURL: 'https://api.mercadopago.com/',
   headers: {
-    Authorization: `Bearer TEST-3840529657724541-080815-fba912c6e91d677be2f3b4e4aa59e138-1445796506`
+    'x-integrator-id': process.env.INTEGRATOR_ID,
+    Authorization: `Bearer ${process.env.AUTH_TOKEN}`
   }
 });
 
