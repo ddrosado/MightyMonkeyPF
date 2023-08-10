c4889f9df51115e3b2a3d571f3929feb30d9889b
matinic <117241248+matinic@users.noreply.github.com>
2023-08-10 10:00:25 -0300

    sport actions

--- client/redux/actions/sportsActions.js
+++ client/redux/actions/sportsActions.js
@@ -7,7 +7,7 @@ export const getSports = createAsyncThunk(
     'sports/getSports',
     async () => {
       try {
-        const response = await axios.get(`${url}/api/sport`);
+        const response = await axios.get(`/api/sport`);
         return response.data;
       } catch (error) {
         throw error.response.data.msg;
@@ -19,7 +19,7 @@ export const getSports = createAsyncThunk(
     'sports/postSports',
     async (obj) => {
       try {
-        const response = await axios.post(`${url}/api/sport`, obj);
+        const response = await axios.post(`/api/sport`, obj);
         return response.data;
       } catch (error) {
         throw new Error(error.response.data.msg)
@@ -42,7 +42,7 @@ export const getSports = createAsyncThunk(
     'sports/putSport',
     async(obj)=>{
       try {
-        return (await axios.put(`${url}/api/sport`, obj)).data
+        return (await axios.put(`/api/sport`, obj)).data
       } catch (error) {
         throw new Error(error.response.data.msg)
       }
